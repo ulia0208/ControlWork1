0799c940aa44d2f06b4079ea4e3d030c61863a5f
Uliana Tokareva <ulia0208@yandex.ru>
2022-11-30 04:19:59 +0300

    Вывод всего массива

--- task/Program.cs
+++ task/Program.cs
@@ -8,11 +8,21 @@ Console.WriteLine("Введите размер массива");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string[] array = new string[n];
-int j=0;
+
 for (int i=0; i<n; i++)
 {
     Console.Write("Введите строку ");
     string m = Console.ReadLine();
-
+    array[i]=m;
 }
 
+
+
+Console.Write("[" );
+for (int i=0; i<n; i++)
+{
+    Console.Write(array[i]+",");
+
+    
+}
+Console.Write("]" );
